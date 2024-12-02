39da52b591ab606d010315e6799c3ea940ea3034
tanihu1 <tanihu2@gmail.com>
2024-12-02 15:03:43 +0200

    Finished project

---
+++ README.md
@@ -0,0 +1,10 @@
+## Small Bootloader demo with x86 Assembly
+
+My attempt at creating a simple x86 bootloader following Joe Bergeron's article.
+
+## Building
+
+```
+nasm -f bin boot.asm -o boot.com
+```
+Can be used to create the binary.
--- boot.asm
+++ boot.asm
@@ -5,6 +5,19 @@
 	mov ss, ax
 	mov sp, 0x2000
 
+	call clearscreen
+
+	push 0x0000
+	call movecursor
+	add sp,2
+
+	push msg
+	call print
+	add sp,2
+
+	cli
+	hlt
+
 clearscreen:
 	push bp
 	mov bp,sp
@@ -37,6 +50,33 @@ movecursor:
 	mov sp, bp
 	pop bp
 	ret
+
+print:
+	push bp,
+	mov bp, sp
+	pusha
+	mov si, [bp+4] ;Pointer to data
+	mov bh, 0x00   ;page number 0, still no double buffering
+	mov bl, 0x00   ;foreground color is ignored in text mode
+	mov ah, 0x0E   ;Print characters to TTY
+
+
+.char:
+	mov al, [si]   ;Get current char from pointer pos
+	add si, 1      ;We want to incremeant untill null terminator
+	or al, 0
+	je .return     ;If string is done, return
+	int 0x10       ;Print character if not done
+	jmp .char      ;loop
+
+.return:
+	popa
+	mov sp,bp
+	pop bp
+	ret
 	
 
-	
+msg: db "Assembly is not so bad.",0
+
+times 510-($-$$) db 0
+dw 0xAA55
